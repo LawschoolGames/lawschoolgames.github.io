ff43f50951e15f9b44f58eba5d8f61922ac91ba8
LawschoolGames <yls2026610126@gmail.com>
2026-09-25 17:47:08 +0900

    Add files via upload

---
+++ Arcade/config.js
@@ -0,0 +1,10 @@
+// 공개용 설정만 입력하세요. 비공개 키는 사용하지 않습니다.
+window.LAW_GAME_CONFIG = {
+  supabaseUrl: "https://ogitsmngtfgkorgjnqum.supabase.co",
+  supabasePublishableKey: "sb_publishable_BkqI-T0VF6ZqT3dAuQZHKA_Ss_COxjV",
+  // AdSense 사이트 승인 및 광고 단위 생성 후 true로 변경하세요.
+  adsenseEnabled: false,
+  adsenseClient: "ca-pub-2988281090144525",
+  adsenseLeftSlot: "",
+  adsenseRightSlot: ""
+};
